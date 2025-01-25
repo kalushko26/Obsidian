0496fdc8de68d0b36e127e594d9fe7eab977cb37
Vadim A. Kalushko <vakalushko.job@yandex.ru>
2025-01-26 00:41:37 +0500

    Прикрутить ЯндексМетрику на сайт (#21)
    [TECH-2025/7]: Прикрутить ЯндексМетрику на сайт

---
+++ quartz/components/ym.tsx
@@ -0,0 +1,36 @@
+import { h } from "preact"
+
+export function YandexMetrika() {
+  return (
+    <>
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(99662287, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });
+          `,
+        }}
+      />
+      <noscript>
+        <div>
+          <img
+            src="https://mc.yandex.ru/watch/99662287"
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
+    </>
+  )
+}
